d91ea2cda410ceb91f85058d683e098ecb2adc6e
zct <zheng7641@163.com>
2019-06-15 13:33:55 +0800

    aaa

---
+++ src/main/java/com/zheng/testeverything/TestController.java
@@ -0,0 +1,28 @@
+package com.zheng.testeverything;
+
+import com.zheng.testeverything.service.TestTransactionService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author zhengchentong on 2019-06-15
+ */
+@RestController
+@RequestMapping("/")
+public class TestController {
+
+    @Autowired
+    private TestTransactionService testTransactionService;
+
+    @RequestMapping("/test1")
+    public String test1() {
+        return testTransactionService.getid();
+    }
+
+    @RequestMapping("/test2")
+    public String test2() throws Exception {
+        testTransactionService.insert();
+        return "ok";
+    }
+}

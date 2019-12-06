d295bf1cef9cc7f59d5717a2d68bf4fac8eaf7b8
zheng7641 <zheng7641@163.com>
2019-12-07 01:21:24 +0800

    wxjava 添加多个公众号

---
+++ src/main/java/com/zheng/testeverything/config/WxMpConfiguration.java
@@ -0,0 +1,32 @@
+package com.zheng.testeverything.config;
+
+import me.chanjar.weixin.mp.api.WxMpService;
+import me.chanjar.weixin.mp.api.impl.WxMpServiceImpl;
+import me.chanjar.weixin.mp.config.WxMpConfigStorage;
+import me.chanjar.weixin.mp.config.impl.WxMpDefaultConfigImpl;
+import org.springframework.context.annotation.Bean;
+import org.springframework.stereotype.Component;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@Component
+public class WxMpConfiguration {
+
+    @Bean
+    public WxMpConfiguration wxMpService() {
+        WxMpService service = new WxMpServiceImpl();
+
+        WxMpDefaultConfigImpl configStorage = new WxMpDefaultConfigImpl();
+        configStorage.setAppId(null);
+        configStorage.setSecret(null);
+        configStorage.setToken(null);
+        configStorage.setAesKey(null);
+
+        Map<String,WxMpConfigStorage> wxMap = new HashMap<>();
+        wxMap.put("appid", configStorage);
+
+        service.setMultiConfigStorages(wxMap);
+        return null;
+    }
+}

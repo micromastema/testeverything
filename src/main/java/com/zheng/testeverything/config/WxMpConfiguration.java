8095eaf80d390a45f6def8f6e90019b2fa381c1f
郑臣通 <chentong.zheng@qingclass.com>
2019-12-10 10:35:40 +0800

    海星脚本

--- src/main/java/com/zheng/testeverything/config/WxMpConfiguration.java
+++ src/main/java/com/zheng/testeverything/config/WxMpConfiguration.java
@@ -17,14 +17,23 @@ public class WxMpConfiguration {
     public WxMpService wxMpService() {
         WxMpService service = new WxMpServiceImpl();
 
-        WxMpDefaultConfigImpl configStorage = new WxMpDefaultConfigImpl();
-        configStorage.setAppId(null);
-        configStorage.setSecret(null);
-        configStorage.setToken(null);
-        configStorage.setAesKey(null);
+        WxMpDefaultConfigImpl ivykidConfigStorage = new WxMpDefaultConfigImpl();
+        ivykidConfigStorage.setAppId("wxc81a45471bf3b23b");
+        ivykidConfigStorage.setSecret("097b97292f056253438b3d8127cc5d72");
+        ivykidConfigStorage.setToken("zgZ8");
+        ivykidConfigStorage.setAesKey("O17RK6I7vEZSkicgeEInXvkQEy4FNIaseiOZofkkz1j");
+        ivykidConfigStorage.setAccessToken("28_t0PzVzBzxeHZ0XmFOc_DsJg_zGtHjpmAg0ybB_AwuyXXeDwjQnXNroeTMSm3WikdP9KEqDvIzeA1_TdARNqHpC0-mGYRve1gscoEBWxVZLYbQj_sQEkb2ZTF31w9U5dV6pEbR2OcJy97roWkUERgAGAYIV");
+
+        WxMpDefaultConfigImpl songshuConfigStorage = new WxMpDefaultConfigImpl();
+        songshuConfigStorage.setAppId("wxd5903083dfc38215");
+        songshuConfigStorage.setSecret("581d6339c7c0a3d52403135ed2b95472");
+        songshuConfigStorage.setToken("zgZ8");
+        songshuConfigStorage.setAesKey("O17RK6I7vEZSkicgeEInXvkQEy4FNIaseiOZofkkz1j");
+        songshuConfigStorage.setAccessToken("28_K2eyrJMCBsQLoTuCcFOfFVsDNI5N0Xl4OgMgqY66jer4vdugxgAXDAqHmA8bA9hYAT0Dd10R4tqDLaC0by0VqzRBsTPurr0OxqTrkg_53o1JLvGMhcaDly5XqkbiDxaxJdczNsCPjYT8fgilDSKfAHANCU");
 
         Map<String,WxMpConfigStorage> wxMap = new HashMap<>();
-        wxMap.put("appid", configStorage);
+        wxMap.put("appid", ivykidConfigStorage);
+        wxMap.put("songshu", songshuConfigStorage);
 
         service.setMultiConfigStorages(wxMap);
         return service;

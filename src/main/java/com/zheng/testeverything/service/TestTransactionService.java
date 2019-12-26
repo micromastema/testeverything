fb673f9d976768293db3fda989f13fc3101950f0
zheng7641 <zheng7641@163.com>
2019-12-26 22:39:19 +0800

    mianshi

--- src/main/java/com/zheng/testeverything/service/TestTransactionService.java
+++ src/main/java/com/zheng/testeverything/service/TestTransactionService.java
@@ -2,10 +2,24 @@ package com.zheng.testeverything.service;
 
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.SortedMap;
+import java.util.TreeMap;
+import java.util.concurrent.ConcurrentSkipListMap;
+
 /**
  * @author zhengchentong on 2019-06-15
  */
 @Service
 public class TestTransactionService {
 
+
+    public void test(){
+        List<String> a = new ArrayList<>();
+
+        SortedMap<String, String> aa = new TreeMap<>();
+        SortedMap<String, String> a1a;
+        ConcurrentSkipListMap<String, String> ab = new ConcurrentSkipListMap<>();
+    }
 }

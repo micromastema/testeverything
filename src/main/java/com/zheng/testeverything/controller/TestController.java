37aee1d12fb11693c6f8a45e5910fddf476d1dc7
zhengchentong <zheng7641@163.com>
2020-04-17 10:42:41 +0800

    prometheus

---
+++ src/main/java/com/zheng/testeverything/controller/TestController.java
@@ -0,0 +1,14 @@
+package com.zheng.testeverything.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author zhengchentong on 2019-06-15
+ */
+@RestController
+@RequestMapping("/")
+public class TestController {
+
+
+}

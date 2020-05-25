3d1cdec7f076e510dff71a12bbf7d273f96fdfa8
zhengchentong <zheng7641@163.com>
2020-05-25 10:30:22 +0800

    成功

--- src/main/java/com/zheng/testeverything/TestController.java
+++ src/main/java/com/zheng/testeverything/TestController.java
@@ -1,23 +1,14 @@
 package com.zheng.testeverything;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-
-
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.sql.Statement;
-import java.util.ArrayList;
-import java.util.List;
-
-import javax.sql.DataSource;
-
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.sql.ResultSet;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * @author zhengchentong on 2019-06-15
@@ -30,15 +21,10 @@ public class TestController {
     @Qualifier("hiveJdbcDataSource")
     org.apache.tomcat.jdbc.pool.DataSource jdbcDataSource;
 
-    @Autowired
-    @Qualifier("hiveDruidDataSource")
-    DataSource druidDataSource;
-
     @RequestMapping("/test2")
     public String test2() throws Exception {
-        List<String> list = new ArrayList<String>();
-         Statement statement = jdbcDataSource.getConnection().createStatement();
-//        Statement statement = druidDataSource.getConnection().createStatement();
+        List<String> list = new ArrayList<>();
+        Statement statement = jdbcDataSource.getConnection().createStatement();
         String sql = "select * from im_pe_im_user limit 10";
         ResultSet res = statement.executeQuery(sql);
         while (res.next()) {
